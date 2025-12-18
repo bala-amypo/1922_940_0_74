22b51ecfdc8a72a7a69c15f2a28e2b6e6cf7d3de
bala-amypo <bala@amypo.in>
2025-12-18 06:35:45 +0000

    User commit

--- src/main/java/com/example/Entity/Student.java
+++ src/main/java/com/example/Entity/Student.java
@@ -1,22 +1,36 @@
-package com.example.demo.entity;
+package com.example.aiml.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 @Entity
 public class Student {
     
-    @Id //Unique Identity and makes the primary key 
+    @Id //Unique identity and makes the primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name filed cannot be empty")
+    @Size(min = 3 , max = 20 , message = "The username must be between 3 and 20 characters")
     private String name;
+    @NotBlank(message = "Invaild email id")
+    @Column(unique = true)
     private String email;
+    
+    //@Notnull
+    //@Size(min,max)
+    //@Min
+    //@Max
+    //@Pattern(reg)
+    //@Postive
     public Long getId() {
         return id;
     }
@@ -36,7 +50,7 @@ public class Student {
         this.email = email;
     
     }
-    public Student(Long id, String name, String email) {
+    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
         this.id = id;
         this.name = name;
         this.email = email;
